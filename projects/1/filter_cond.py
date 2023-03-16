bb5ceda65d4ba6027a6b4568ae104191139c529a
dmsy4 <demsy2109@gmail.com>
2023-03-16 01:04:04 +0000

    proj 1

---
+++ projects/1/filter_cond.py
@@ -0,0 +1,14 @@
+#
+#
+def filter_cond(line_dict):
+    """Filter function
+    Takes a dict with field names as argument
+    Returns True if conditions are satisfied
+    """
+    if line_dict["if1"] == '':
+        cond_match = False
+    else:
+        cond_match = (
+        20 < int(line_dict["if1"]) < 40
+        ) 
+    return True if cond_match else False
---
+++ projects/1/predict.py
@@ -0,0 +1,39 @@
+#!/opt/conda/envs/dsenv/bin/python
+
+import sys, os
+import logging
+from joblib import load
+import pandas as pd
+
+sys.path.append('.')
+from model import fields
+
+#
+# Init the logger
+#
+logging.basicConfig(level=logging.DEBUG)
+logging.info("CURRENT_DIR {}".format(os.getcwd()))
+logging.info("SCRIPT CALLED AS {}".format(sys.argv[0]))
+logging.info("ARGS {}".format(sys.argv[1:]))
+
+#load the model
+model = load("1.joblib")
+
+#fields = """doc_id,hotel_name,hotel_url,street,city,state,country,zip,class,price,
+#num_reviews,CLEANLINESS,ROOM,SERVICE,LOCATION,VALUE,COMFORT,overall_ratingsource""".replace("\n",'').split(",")
+
+#read and infere
+# read_opts=dict(
+#         sep=',', names=fields[0] + fields[2:], index_col=False, header=None,
+#         iterator=True, chunksize=100
+# )
+
+read_opts = dict(sep="\t", names=fields[0] + fields[2:], index_col=False, header=None,
+        iterator=True, chunksize=100)
+df = pd.read_table(train_path, **read_table_opts)
+
+
+for df in pd.read_csv(sys.stdin, **read_opts):
+    pred = model.predict_proba(df.iloc[:, 1:])
+    out = zip(df.id, pred)
+    print("\n".join(["{0},{1}".format(*i) for i in out]))
